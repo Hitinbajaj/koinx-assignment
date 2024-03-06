904ebf26ddda8c186a57161d2c9d71cd1388487b
Hitinbajaj <hitinbajaj77@gmail.com>
2024-03-06 23:51:20 +0530

    Navbar responsive

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,21 @@
-
+import Dashboard from "../components/Dashboard"
+import PageHolder from "../components/PageHolder"
+import Navbar from "../components/Navbar"
 function MainPage() {
   return (
-    <div>
-      
-    </div>
+    <div className="h-full w-screen bg-slate-200/40">
+        <Navbar></Navbar>
+        <PageHolder/>
+
+        <div className="w-screen lg:flex">
+            <div className="lg:w-8/12 lg:ml-14 mx-4">
+                <Dashboard/>
+            </div>
+            <div className="lg:w-4/12 lg:mr-14 mx-4">
+                
+            </div>
+        </div>
+  </div>
   )
 }
 
