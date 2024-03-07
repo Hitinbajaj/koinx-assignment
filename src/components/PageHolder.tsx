4ff460c985f461850bc79c8a461fe1c09029bd79
Hitinbajaj <hitinbajaj77@gmail.com>
2024-03-07 21:31:56 +0530

    Deplpyement bugs

--- src/components/PageHolder.tsx
+++ src/components/PageHolder.tsx
@@ -1,30 +1,41 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
-interface PageHolderProps {
-  currentPage?: string;
-}
+interface PageHolderProps {}
 
-const PageHolder: React.FC<PageHolderProps> = ({ currentPage = 'Bitcoin' }) => {
+const PageHolder: React.FC<PageHolderProps> = () => {
+  const location = useLocation();
+
   return (
     <div className="flex lg:pl-14 ml-4 pt-4">
-      <div className="text-sm text-[#3E5765]">
-        Cryptocurrencies
-      </div>
-      <div className="text-sm text-[#3E5765]">
-        {' >> '} 
-      </div>
-      <NavLink to="/btc" className={`text-md text-${currentPage === 'Bitcoin' ? 'blue-500' : '#0F1629'} mr-3 ml-3`}>
+      <div className="text-sm text-[#3E5765]">Cryptocurrencies</div>
+      <div className="text-sm text-[#3E5765]">{' >> '}</div>
+      <NavLink
+        to="/btc"
+        className={`text-md mr-3 ml-3 ${
+          location.pathname === '/btc' ? 'text-blue-500 border-b-2 border-blue-500' : 'text-gray-700'
+        }`}
+      >
         Bitcoin
       </NavLink>
-      <NavLink to="/eth" className={`text-md text-${currentPage === 'Ethereum' ? 'blue-500' : '#0F1629'} mr-3`}>
+      <NavLink
+        to="/eth"
+        className={`text-md mr-3 ${
+          location.pathname === '/eth' ? 'text-blue-500 border-b-2 border-blue-500' : 'text-gray-700'
+        }`}
+      >
         Ethereum
       </NavLink>
-      <NavLink to="/opul" className={`text-md text-${currentPage === 'Opulous' ? 'blue-500' : '#0F1629'}`}>
+      <NavLink
+        to="/opul"
+        className={`text-md ${
+          location.pathname === '/opul' ? 'text-blue-500 border-b-2 border-blue-500' : 'text-gray-700'
+        }`}
+      >
         Opulous
       </NavLink>
     </div>
   );
-}
+};
 
 export default PageHolder;
